a59a23d7144aff92bf08950ebe0c7af715cafb6c
Suparat Saelim <114087424+NKmmii02@users.noreply.github.com>
2024-06-19 18:58:50 +0700

    Create Brand.js

---
+++ src/components/Brand.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Brand = () => {
+    return (
+        <div>
+            <h1>แบรนด์</h1>
+            <p>เนื้อหาของแบรนด์</p>
+        </div>
+    );
+}
+
+export default Brand;
