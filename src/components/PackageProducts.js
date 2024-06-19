03eb9e202eb17c538c4d48ae186a52be1d6a1ce8
Suparat Saelim <114087424+NKmmii02@users.noreply.github.com>
2024-06-19 19:00:03 +0700

    Create PackageProducts.js

---
+++ src/components/PackageProducts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PackageProducts = () => {
+    return (
+        <div>
+            <h1>สินค้าตามแพ็กเกจ</h1>
+            <p>เนื้อหาของสินค้าตามแพ็กเกจ</p>
+        </div>
+    );
+}
+
+export default PackageProducts;
