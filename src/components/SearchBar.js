7f53b0ffae8d36b032aaedda91434e2977759cee
SRTYMK <114692686+SRTYMK@users.noreply.github.com>
2024-06-25 11:27:01 +0700

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SearchBar.css';
+import '../App.css';
 
 const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState('');
