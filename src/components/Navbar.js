2163ebe5467966f07180968b8247da22eda883b2
SRTYMK <114692686+SRTYMK@users.noreply.github.com>
2024-06-27 02:21:14 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const Navbar = ({ openAuthPopup }) => {
+const Navbar = ({ openAuthPopup, isLoggedIn, userId, onLogout }) => {
     return (
         <nav className="navbar">
             <div className='logo-container'>
@@ -17,9 +17,8 @@ const Navbar = ({ openAuthPopup }) => {
                     <Link to='/แบรนด์'>แบรนด์</Link>
                     <div className="dropdown-content">
                         <ul>
-                            <li><Link to='/brand1'>แบรนด์ 1</Link></li>
-                            <li><Link to='/brand2'>แบรนด์ 2</Link></li>
-                            <li><Link to='/brand3'>แบรนด์ 3</Link></li>
+                            <li><Link to='https://www.chiataigroup.com/'>CHIATAI</Link></li>
+                            <li><Link to='https://sorndaengseed.com/'>ศรแดง</Link></li>
                         </ul>
                     </div>
                 </li>
@@ -27,27 +26,26 @@ const Navbar = ({ openAuthPopup }) => {
                     <Link to='/ประเภทเมล็ดพันธุ์'>ประเภทเมล็ดพันธุ์</Link>
                     <div className="dropdown-content">
                         <ul>
-                            <li><Link to='/seedType1'>ประเภท 1</Link></li>
-                            <li><Link to='/seedType2'>ประเภท 2</Link></li>
-                            <li><Link to='/seedType3'>ประเภท 3</Link></li>
-                        </ul>
-                    </div>
-                </li>
-                <li className="dropdown">
-                    <Link to='/สินค้าตามแพ็กเกจ'>สินค้าตามแพ็กเกจ</Link>
-                    <div className="dropdown-content">
-                        <ul>
-                            <li><Link to='/packageProducts1'>แพ็กเกจ 1</Link></li>
-                            <li><Link to='/packageProducts2'>แพ็กเกจ 2</Link></li>
-                            <li><Link to='/packageProducts3'>แพ็กเกจ 3</Link></li>
+                            <li><Link to='/seedType/F1'> F1 </Link></li>
+                            <li><Link to='/seedType/OP'>OP</Link></li>
                         </ul>
                     </div>
                 </li>
             </ul>
             <div className="auth-links">
-                <button onClick={openAuthPopup} className="auth-link">
-                    <i className='fas fa-user'></i><span>Login/Sign Up</span>
-                </button>
+                {isLoggedIn ? (
+                    <div className="user-dropdown">
+                        <i className='fas fa-user'></i>
+                        <div className="user-dropdown-content">
+                            <Link to={`/User/${userId}`} className="user-profile-link">Profile</Link>
+                            <button onClick={onLogout} className="logout-button">Logout</button>
+                        </div>
+                    </div>
+                ) : (
+                    <button onClick={openAuthPopup} className="auth-link">
+                        <i className='fas fa-user'></i><span>Login/Sign Up</span>
+                    </button>
+                )}
             </div>
         </nav>
     );
